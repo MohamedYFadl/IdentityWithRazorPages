f6b47042ead6b38e72dc966d6d6601d5abff33f5
MomoYasser0 <myasoo28@gmail.com>
2024-10-07 15:51:43 +0300

    Edit Connection String & Show Hidden Views & Rename Identity Tables

--- IdentityWithRazorPages/Data/Migrations/20241007124915_RenameTables.Designer.cs
+++ IdentityWithRazorPages/Data/Migrations/20241007124915_RenameTables.Designer.cs
@@ -4,14 +4,16 @@ using IdentityWithRazorPages.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityWithRazorPages.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241007124915_RenameTables")]
+    partial class RenameTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
